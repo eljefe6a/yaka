727b34372a070ce6197b4db9eb1626f66edff766
Jesse Anderson <eljefe6a@gmail.com>
2018-07-23 07:36:59 -0500

    Changed DataListener to interface so it can be lambda

--- src/main/java/io/bigdatainstitute/yaka/listener/DataListener.java
+++ src/main/java/io/bigdatainstitute/yaka/listener/DataListener.java
@@ -1,5 +1,5 @@
 package io.bigdatainstitute.yaka.listener;
 
-public abstract class DataListener<K, V> {
-	public abstract void dataReceived(K key, V value);
+public interface DataListener<K, V> {
+	public void dataReceived(K key, V value);
 }
